c4dd2d3d2ded961cb9cee8dc3917f1bb037a3748
vijaya <agraharapuvijaylakshmi248@gmail.com>
2023-12-31 20:01:26 +0530

    Your commit message

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const dbPath = path.join(__dirname,"tastykitchen.db");
+const sqlite3 = require("sqlite3");
+const {open} = require("sqlite");
+
+let db = null
+
+const restaurant = require("./restaurantDetails.json")
+const offers = restaurant.offers
+
+const initailizeDbAndServer = async ()=>{
+    try{
+        db = await open({
+            filename:dbPath,
+            driver:sqlite3.Database,
+        })
+        app.listen(4000,()=>{
+            console.log("server running at https://localhost:4000/")
+        })
+    }
+    catch(err){
+        console.log(`DB Error:${err.message}`)
+        process.exit(1)
+    }
+}
+
+initailizeDbAndServer();
+
+
+
+
+app.get("/offers",async (req,res) => {
+    const getQuery = `SELECT * FROM offers;`
+    const data = await db.all(getQuery)
+    res.send(data)
+})
